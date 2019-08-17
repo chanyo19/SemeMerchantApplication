dcd038fa0b446953f6551195ca34e6164473e51c
shashila heshan <ebayshashila@gmail.com>
2019-08-17 11:26:34 +0530

    changed merchant_services table service_id->services_id

--- database/migrations/2019_08_14_120657_create_merchant_service_table.php
+++ database/migrations/2019_08_14_120657_create_merchant_service_table.php
@@ -16,11 +16,11 @@ class CreateMerchantServiceTable extends Migration
         Schema::create('merchant_services', function (Blueprint $table) {
             $table->primary(['merchant_id','service_id']);
             $table->integer('merchant_id')->unsigned();
-            $table->integer('service_id')->unsigned();
+            $table->integer('services_id')->unsigned();
             $table->double('price')->default(0);
             $table->timestamps();
             $table->foreign('merchant_id')->references('id')->on('merchants')->onDelete('cascade');
-            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
+            $table->foreign('services_id')->references('id')->on('services')->onDelete('cascade');
         });
     }
 
