bb8fdef2542cf6fb28ac7f6c001381043be14756
shashila heshan <ebayshashila@gmail.com>
2019-08-17 20:38:09 +0530

    time-slot-add-seeder-added

--- database/seeds/timeslot.php
+++ database/seeds/timeslot.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class timeslot extends Seeder
 {
@@ -11,6 +12,50 @@ class timeslot extends Seeder
      */
     public function run()
     {
-        //
+       $this->addslotinfo();
+
+    }
+
+    public function addslotinfo(){
+        DB::table('timeslots')->insert([
+            'slot_info' => '8-9',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '9-10',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '10-11',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '11-12',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '12-13',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '13-14',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '14-15',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '15-16',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '16-17',
+
+        ]);
+        DB::table('timeslots')->insert([
+            'slot_info' => '17-18',
+
+        ]);
     }
 }
