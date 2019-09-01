6cc224b5554200646393cf8c88d3d7be8582a9ee
shashila heshan <ebayshashila@gmail.com>
2019-09-01 16:17:37 +0530

    test mail queue added

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class TestJob implements ShouldQueue
 {
@@ -30,6 +31,17 @@ class TestJob implements ShouldQueue
      */
     public function handle()
     {
-        Log::info('hello queue executed');
+        $this->basic_email();
+
+    }
+    public function basic_email() {
+        $data = array('name'=>"Shashila heshan");
+
+        Mail::send(['text'=>'mail'], $data, function($message) {
+            $message->to('abc@gmail.com', 'Tutorials Point')->subject
+            ('Laravel Basic Testing Mail');
+            $message->from('xyz@gmail.com','shashila heshan');
+        });
+        Log::info("Basic Email Sent. Check your inbox.");
     }
 }
