e0c2d471f63ba37717143335abc819c0df73be00
shashila heshan <ebayshashila@gmail.com>
2019-09-01 16:36:34 +0530

    test mail queue added

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -34,16 +34,16 @@ class TestJob implements ShouldQueue
     {
         $data = array('name'=>"Shashila heshan");
         Log::info("Sending mail");
-        try{
-            Mail::send('mail.mail', $data, function($message) {
-                $message->to(' a08fc27b67-9093ae@inbox.mailtrap.io', 'Tutorials Point')->subject
-                ('Laravel Basic Testing Mail');
-                $message->from('xyz@gmail.com','shashila heshan');
-            });
-            Log::info("Basic Email Sent. Check your inbox.");
-        }catch (\Exception $exception){
-            Log::error('failed '.$exception);
-        }
+//        try{
+//            Mail::send('mail.mail', $data, function($message) {
+//                $message->to(' a08fc27b67-9093ae@inbox.mailtrap.io', 'Tutorials Point')->subject
+//                ('Laravel Basic Testing Mail');
+//                $message->from('xyz@gmail.com','shashila heshan');
+//            });
+//            Log::info("Basic Email Sent. Check your inbox.");
+//        }catch (\Exception $exception){
+//            Log::error('failed '.$exception);
+//        }
 
     }
 
